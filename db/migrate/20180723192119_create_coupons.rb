d8de9b90d9e7c09ec6df7d9d7ed03c7575bdc55b
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-23 15:21:32 -0400

    add Coupon resource

---
+++ db/migrate/20180723192119_create_coupons.rb
@@ -0,0 +1,12 @@
+class CreateCoupons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :coupons do |t|
+      t.string :coupon_code
+      t.datetime :expiration_date
+      t.text :offer_description
+      t.belongs_to :store, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
