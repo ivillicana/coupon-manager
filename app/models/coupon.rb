3355e33d6d63c06efd935dbddc855ae7f8593a11
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-23 15:26:13 -0400

    add model associations

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,5 @@
 class Coupon < ApplicationRecord
   belongs_to :store
+  has_many :users, through: :user_coupons
+  has_many :user_coupons
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_coupons
+  has_many :coupons, through: :user_coupons
+  has_many :stores, through: :coupons
+  
+  has_secure_password
 end
