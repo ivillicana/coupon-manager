662fd66c577a8b9d8a49e1f368654a09c4f730c4
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-24 13:02:06 -0400

    add show action

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,6 +1,11 @@
 class StoresController < ApplicationController
+  before_action :find_store, only: [:show, :edit, :update, :destroy]
 
   def index
     @stores = Store.all
+  end
+
+  def show
+    
   end
 end
