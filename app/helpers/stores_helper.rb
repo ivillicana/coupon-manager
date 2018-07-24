92fe92cdd17eb528cb2992c927947d6dff85fc00
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-24 13:18:36 -0400

    add store_coupon_count helper method

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -2,4 +2,8 @@ module StoresHelper
   def store_coupons(store)
     store.coupons
   end
+
+  def store_coupon_count(store)
+    "#{store.coupons.count} #{"coupon".pluralize(store.coupons.count)}"
+  end
 end
