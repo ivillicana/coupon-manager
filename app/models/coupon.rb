de564467c6c727ad5d75c30de8fbe5288729b991
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-24 10:56:23 -0400

    add methods for store_name=

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,4 +2,12 @@ class Coupon < ApplicationRecord
   belongs_to :store
   has_many :users, through: :user_coupons
   has_many :user_coupons
+
+  def store_name=(args)
+    self.store = Store.find_or_create_by(name: args)
+  end
+
+  def store_name
+    self.store ? self.store.name : nil
+  end
 end
