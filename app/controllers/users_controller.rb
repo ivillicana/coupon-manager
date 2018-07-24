e9d8a3678991404d2f3e5cf917e9a1d63fc9c2a0
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-23 20:18:10 -0400

    add create action with user_params and downcase username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
   def new
-    
+    @user = User.new
   end
 
   def create
-    
+    if all_user_params_filled?
+      downcase_username
+      @user = User.create(user_params)
+      redirect_to coupons_path, notice: "User successfully created!"
+    else
+      render :new, alert: "Please correct errors errors"
+    end
   end
 
   private
@@ -13,4 +19,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :username)
   end
+
+  def all_user_params_filled?
+    user_params.to_h.all? { |k, v| !v.empty?}
+  end
+
+  def downcase_username
+    user_params[:username].downcase!
+  end
+
+
 end
