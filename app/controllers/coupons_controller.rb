daa44feda0e22af0ecb91fa8f0f12a0655be4b08
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-23 21:16:03 -0400

    add params method and upcase code

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -11,4 +11,18 @@ class CouponsController < ApplicationController
   def create
     
   end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:coupon_code, :expiration_date, :offer_description )
+  end
+
+  def upcase_coupon_code
+    coupon_params[:coupon_code].upcase!
+  end
+
+  def all_coupon_params_filled?
+    coupon_params.to_h.all? { |k, v| !v.empty?}
+  end
 end
