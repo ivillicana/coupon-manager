bdf742d107bd5986e3a8866c2d7817157ce55992
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-24 14:46:02 -0400

    add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'application#home'
   resources :coupons
   resources :stores
   resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
