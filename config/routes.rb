745d0484863711d89625badd218f99231ce31d95
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-25 12:39:02 -0400

    add omniauth.rb and corresponding route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+  get '/auth/facebook/callback' => 'sessions#create_from_facebook'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
