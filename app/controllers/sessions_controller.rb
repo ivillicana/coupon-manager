31f1eb12f071050538eeadd0373de3f098afb4e0
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-25 12:36:53 -0400

    add omniauth authentication and action to sesssions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,18 @@ class SessionsController < ApplicationController
     redirect_to user_path(@user), alert: "Welcome #{@user.name}"
   end
 
+  def create_from_facebook
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+      u.password = auth['uid']
+    end
+ 
+    session[:user_id] = @user.id
+    redirect_to user_path(@user), alert: "Welcome #{@user.name}"
+  end
+
   def destroy
     session.delete :user_id
     return redirect_to login_path
@@ -23,6 +35,10 @@ class SessionsController < ApplicationController
 
   def login_params_empty?
     !params[:user][:username] || params[:user][:username].empty?
+  end
+
+  def auth
+    request.env['omniauth.auth']
   end
   
 end
