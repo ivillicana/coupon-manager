f70275b6f8280ace318925615c7c840016f64096
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-25 17:55:20 -0400

    add store_id_field helper method

--- app/helpers/coupons_helper.rb
+++ app/helpers/coupons_helper.rb
@@ -1,2 +1,10 @@
 module CouponsHelper
+
+  def store_id_field(coupon, f)
+    if coupon.store.nil?
+      render partial: 'store_datalist', locals: {f: f}
+    else
+      hidden_field_tag "coupon[store_id]", coupon.store_id
+    end
+  end
 end
