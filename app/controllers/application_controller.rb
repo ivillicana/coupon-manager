c66b80fd07a91dd5f34b69fbf14a88445e333834
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-25 11:25:53 -0400

    move logged_in? to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   def home
     
   end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def redirect_if_not_logged_in
+    return redirect_to login_path, alert: "You must be logged in" if !logged_in?
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,14 +40,6 @@ class UsersController < ApplicationController
     user_params[:username].downcase!
   end
 
-  def logged_in?
-    !!session[:user_id]
-  end
-
-  def redirect_if_not_logged_in
-    return redirect_to login_path, alert: "You must be logged in" if !logged_in?
-  end
-
   def redirect_if_logged_in
     return redirect_to user_path(session[:user_id]), alert: "You are already logged in" if logged_in?
   end
