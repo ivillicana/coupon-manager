d9887e53be8083eb7ed1007bf9abdff6e13a2717
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-31 11:48:52 -0400

    style with bootstrap

--- app/helpers/coupons_helper.rb
+++ app/helpers/coupons_helper.rb
@@ -5,7 +5,7 @@ module CouponsHelper
       render partial: 'store_datalist', locals: {f: f}
     else
       hidden_field_tag "coupon[store_id]", coupon.store_id
-      label_tag 'coupon_store_id', coupon.store.name
+      label_tag 'coupon_store_id', coupon.store.name, class: "h5"
     end
   end
 end
