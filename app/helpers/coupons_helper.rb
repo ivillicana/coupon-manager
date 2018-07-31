9270701b3f41b59073c6ef2bb37542b08fa5c8e2
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-31 11:54:20 -0400

    refactor save_or_delete_from_profile button into coupon helper method

--- app/helpers/coupons_helper.rb
+++ app/helpers/coupons_helper.rb
@@ -8,4 +8,17 @@ module CouponsHelper
       label_tag 'coupon_store_id', coupon.store.name, class: "h5"
     end
   end
+
+  def save_or_delete_from_profile(coupon)
+    if current_user.coupons.include? (coupon) 
+      form_tag delete_coupon_path(coupon), class: "form-signin login-2" do 
+        submit_tag 'Delete from my Profile', class: "btn btn-sm btn-secondary btn-block" 
+      end 
+    else 
+      form_tag save_coupon_path(coupon), class: "form-signin login-2" do 
+        submit_tag 'Save to my Profile', class: "btn btn-sm btn-primary btn-block" 
+      end 
+    end 
+  end
+
 end
