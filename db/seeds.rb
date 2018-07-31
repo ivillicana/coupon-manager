61bd03ad06ce053669bd9466dadc04ae4d1ad3d5
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-31 12:57:38 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Donald Trump", email: "dtrump@gmail.com", password: "password")
+User.create(name: "Barack Obama", email: "bobama@gmail.com", password: "password")
+
+Coupon.create(
+  "coupon_code"=>"2FOR5",
+  "expiration_date"=>"Fri, 31 Aug 2018",
+  "offer_description"=>"2 Kombucha drinks for $5",
+  "store_name"=>"Target",
+  "item"=>"Kombucha")
+Coupon.create(
+  "coupon_code"=>"5ABC54",
+  "expiration_date"=>"Mon, 13 Aug 2018",
+  "offer_description"=>"$8 per pound",
+  "store_name"=>"Aldi",
+  "item"=>"Steak")
+Coupon.create(
+  "coupon_code"=>"COUPON1123",
+  "expiration_date"=>"Wed, 15 Aug 2018",
+  "offer_description"=>"Buy 1 get one free",
+  "store_name"=>"Aldi",
+  "item"=>"Water")
+Coupon.create(
+  "coupon_code"=>"1OFFTOILETPAPER",
+ "expiration_date"=>"Mon, 30 Jul 2018",
+ "offer_description"=>"$1 off",
+ "store_name"=>"Target",
+ "item"=>"Toilet Paper"
+)
+Coupon.create(
+  "coupon_code"=>"FREEBAG2018",
+  "expiration_date"=>"Wed, 05 Sep 2018",
+  "offer_description"=>"Free Starbucks coffee bag",
+  "store_name"=>"Target",
+  "item"=>"Starbucks")
