414c661713f5416e0ad2e703874da7c0d258aaf9
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-07-30 22:13:02 -0400

    stylize coupon attributes before saving

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,12 +3,12 @@ class Coupon < ApplicationRecord
   has_many :user_coupons
   has_many :users, through: :user_coupons
   
-
+  before_save :stylize_attributes
   validates :item, :coupon_code, :offer_description, :expiration_date, :store_id, presence: true
   validates :coupon_code, format: {with: /\A[^\s]+\z/, message: "must not have spaces"}
 
   def store_name=(args)
-    self.store = Store.find_or_create_by(name: args.capitalize)
+    self.store = Store.find_or_create_by(name: args.split.each {|w| w.capitalize!}.join(" "))
   end
 
   def store_name
@@ -41,4 +41,10 @@ class Coupon < ApplicationRecord
       "Expires in #{difference_days} #{"day".pluralize(difference_days)}"
     end
   end
+
+  def stylize_attributes
+    self.item = self.item.split.each {|w| w.capitalize!}.join(" ")
+    self.coupon_code = self.coupon_code.upcase!
+  end
+  
 end
