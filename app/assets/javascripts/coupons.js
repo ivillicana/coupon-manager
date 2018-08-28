c319a1420173a129e33c0aa86e9e35154b9d40f2
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-08-28 17:02:21 -0400

    add handlebars templates and assets

--- app/assets/javascripts/coupons.js
+++ app/assets/javascripts/coupons.js
@@ -1,14 +1,22 @@
 $(function(){
-
   attachEventListeners();
-
 })
 
-
+function requestCoupons() {
+  
+}
+function loadCouponsTemplate() {
+  $.get('/coupons', (coupons) => {
+    $('#saved-coupons').empty();
+    var couponsHTML = HandlebarsTemplates['coupons_template'](coupons)
+    $('#coupons-list').html(couponsHTML)
+  })
+  
+}
 
 function attachEventListeners() {
   $('#coupons-nav-button').on('click', function (e){
     e.preventDefault();
-    requestCoupons();
+    loadCouponsTemplate();
   })
 }
