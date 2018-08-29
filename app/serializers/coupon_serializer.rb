e6f9b3e16535b2ffa4b65f57dc1c8c67538b895a
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-08-29 12:16:53 -0400

    add expires_in to user serializer attributes

--- app/serializers/coupon_serializer.rb
+++ app/serializers/coupon_serializer.rb
@@ -1,4 +1,4 @@
 class CouponSerializer < ActiveModel::Serializer
-  attributes :id, :coupon_code, :offer_description, :item, :expiration_date, :store_name
+  attributes :id, :coupon_code, :offer_description, :item, :expiration_date, :store_name, :expires_in
   belongs_to :store
 end
