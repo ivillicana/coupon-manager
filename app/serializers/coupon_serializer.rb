ccb09adcd90105a8171fc6bd05cde6eff3ad773d
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-08-29 12:02:13 -0400

    add store_name to attributes

--- app/serializers/coupon_serializer.rb
+++ app/serializers/coupon_serializer.rb
@@ -1,4 +1,4 @@
 class CouponSerializer < ActiveModel::Serializer
-  attributes :id, :coupon_code, :offer_description, :item, :expiration_date
+  attributes :id, :coupon_code, :offer_description, :item, :expiration_date, :store_name
   belongs_to :store
 end
