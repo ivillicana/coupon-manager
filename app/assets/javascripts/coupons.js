62ca9f069a3419ea960a9b8369ead4947d965f72
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-08-28 20:35:49 -0400

    add stores template

--- app/assets/javascripts/coupons.js
+++ app/assets/javascripts/coupons.js
@@ -7,13 +7,28 @@ function loadCoupons() {
   $.get('/coupons', (coupons) => {
     var couponsHTML = HandlebarsTemplates['coupons_template'](coupons)
     $('#display').html(couponsHTML)
-    $('a.coupon-link').on('click', () => {debugger; loadCoupon(this)})
+    $('.coupon-link').on('click', function(){
+      loadCoupon(this)
+    })
   })
+}
+
+function loadCoupon(coupon) {
   
 }
 
 function loadStores() {
+  $.get('/stores', (stores) => {
+    var storesHTML = HandlebarsTemplates['stores_template'](stores)
+    $('#display').html(couponsHTML)
+    $('.store-link').on('click', function(){
+      loadStore(this)
+    })
+  })
+}
 
+function loadStore(store) {
+  
 }
 
 function attachEventListeners() {
