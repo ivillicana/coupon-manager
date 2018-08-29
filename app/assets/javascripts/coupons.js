34a2535c7cd8e695dd7c490648b5fd1d8b0a7902
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-08-28 21:14:34 -0400

    add loadStore();

--- app/assets/javascripts/coupons.js
+++ app/assets/javascripts/coupons.js
@@ -7,28 +7,44 @@ function loadCoupons() {
   $.get('/coupons', (coupons) => {
     var couponsHTML = HandlebarsTemplates['coupons_template'](coupons)
     $('#display').html(couponsHTML)
-    $('.coupon-link').on('click', function(){
-      loadCoupon(this)
+    $('.coupon-link').on('click', function(e){
+      e.preventDefault();
+      loadCoupon(this);
     })
   })
 }
 
 function loadCoupon(coupon) {
-  
+  $.get(`/coupons/${coupon.dataset.couponid}`, (data) => {
+    var couponHTML = HandlebarsTemplates['coupon_template'](data)
+    $('#display').html(couponHTML);
+    $('.store-link').on('click', function(e){
+      e.preventDefault();
+      loadStore(this);
+    })
+  })
 }
 
 function loadStores() {
   $.get('/stores', (stores) => {
     var storesHTML = HandlebarsTemplates['stores_template'](stores)
-    $('#display').html(couponsHTML)
-    $('.store-link').on('click', function(){
-      loadStore(this)
+    $('#display').html(storesHTML)
+    $('.store-link').on('click', function(e){
+      e.preventDefault();
+      loadStore(this);
     })
   })
 }
 
 function loadStore(store) {
-  
+  $.get(`/stores/${store.dataset.storeid}`, (data) => {
+    var storeHTML = HandlebarsTemplates['store_template'](data)
+    $('#display').html(storeHTML);
+    $('.coupon-link').on('click', function(e){
+      e.preventDefault();
+      loadCoupon(this);
+    })
+  })
 }
 
 function attachEventListeners() {
