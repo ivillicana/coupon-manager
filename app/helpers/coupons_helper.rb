f3ffa9a21f993ea1915035826b21c7a70d57be28
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-03 21:22:07 -0400

    fix return value of #store_id_field to return both form tags

--- app/helpers/coupons_helper.rb
+++ app/helpers/coupons_helper.rb
@@ -4,8 +4,7 @@ module CouponsHelper
     if coupon.store.nil?
       render partial: 'store_datalist', locals: {f: f}
     else
-      hidden_field_tag "coupon[store_id]", coupon.store_id
-      label_tag 'coupon_store_id', coupon.store.name, class: "h5"
+      return (label_tag 'coupon_store_id', coupon.store.name, class: "h5") + (hidden_field_tag "coupon[store_id]", coupon.store_id)
     end
   end
 
