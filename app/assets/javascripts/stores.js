9eccbc58ba67b9ccc4463bc78d7bd08a1860d17d
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-10 17:31:43 -0400

    update stores template

--- app/assets/javascripts/stores.js
+++ app/assets/javascripts/stores.js
@@ -15,16 +15,45 @@ class Store {
 
 }
 
+let storeObjects = []
+
 function loadStores() {
   $.get($('#profile-nav-button')[0].href).done((userData) => {
     userObject = new User(userData)
   })
   $.get('/stores', (stores) => {
+    storeObjects.length = 0;
+      stores.forEach( (store) => { 
+        let newStore = new Store(store); 
+        storeObjects.push(newStore); 
+      })
+      
+      
+      
     var storesHTML = HandlebarsTemplates['stores_template'](stores)
     $('#display').html(storesHTML)
     addStoreLinkListener();
     $('.store-coupons-link').on('click', function(e){
       previewStoreCoupons(this);
+    })
+
+    $('#reverse-sort').on('click', function() {
+      storeObjects.sort(function(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return 1;
+        }
+        if (nameA > nameB) {
+          return -1;
+        }
+      
+        // names must be equal
+        return 0;
+      });
+      
+      var storesHTML = HandlebarsTemplates['stores_template'](storeObjects)
+      $('#display').html(storesHTML)
     })
   })
 }
